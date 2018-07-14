a1ca69e59560cd5bf976a271569358847cc471a1
Anoob Backer <anoob.backer@live.com>
2018-07-14 18:17:20 +0530

    Prevent caching.

--- js/bet.js
+++ js/bet.js
@@ -6,8 +6,8 @@ var parser;
 var pauseChecked = false;
 var printStepChecked = false;
 var matchResults = null;
-var resultsURL = "https://fabrikamsa1.blob.core.windows.net/wc2018/results.csv?ver=3.2";
-var predictionDataURL = "https://fabrikamsa1.blob.core.windows.net/wc2018/predict.csv?ver=4.0";
+var resultsURL = "https://fabrikamsa1.blob.core.windows.net/wc2018/results.csv?ver=3.3";
+var predictionDataURL = "https://fabrikamsa1.blob.core.windows.net/wc2018/predict.csv?ver=4.1";
 
 var matchStages = [
   //Groupstage match day1
